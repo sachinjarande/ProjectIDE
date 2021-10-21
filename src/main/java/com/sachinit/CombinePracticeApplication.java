74bdebe4f4785caa9c44e8848e867575eb5cdcee
sachinjarande <sachinjarande71093@gmail.com>
2021-10-21 14:21:53 +0530

    IES:01 Model class created

---
+++ src/main/java/com/sachinit/CombinePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sachinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CombinePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CombinePracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sachinit/CombinePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombinePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
