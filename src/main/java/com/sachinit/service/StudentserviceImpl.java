b1dbaa5566fd27a8a296df89cf422b5142a83d6d
MangeshRajgure <mangeshrajgure@gmail.com>
2021-10-21 14:56:39 -0700

    IES:02 service pakage created and studentserviceI interface and StudentsrviceImpl class created and m1 method added

---
+++ src/main/java/com/sachinit/service/StudentserviceImpl.java
@@ -0,0 +1,12 @@
+package com.sachinit.service;
+
+public class StudentserviceImpl implements StudentserviceI{
+
+	@Override
+	public void m1() {
+		System.out.println("Welcome to Sachinit");
+		
+	}
+	
+
+}
